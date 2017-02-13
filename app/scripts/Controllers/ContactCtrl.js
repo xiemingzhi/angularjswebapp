0e45e7a9af4b2762a68e8f9eec441fbf166bc58c
ming <xiemingzhi@gmail.com>
2017-02-13 23:06:48 +0800

    AngularJS Basic Auth Example Must enable CORS on server side

--- app/scripts/Controllers/ContactCtrl.js
+++ app/scripts/Controllers/ContactCtrl.js
@@ -1,7 +1,10 @@
 "use strict";
 angular.module('AngExample')
     .controller('ContactCtrl', function ($scope, $http) {
-    $http.get('https://pelagic-logic-648.appspot.com/contact/jsonlist.json').
+    //connect to server on a different domain
+    $http.defaults.headers.common = {"Access-Control-Request-Headers": "accept, origin, authorization"}; 
+    $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa('admin' + ':' + 'password');//encode
+    $http.get('http://localhost:8000/userman/services/api/users?_type=json').
         success(function(data) {
             $scope.contactData = data;
         });
